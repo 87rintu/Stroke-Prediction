2549c22a0f6b5b6120c991fcce55c873aad32d0c
87rintu <87.rintu@gmail.com>
2020-07-03 19:02:35 +0530

    Add files via upload

--- request.py
+++ request.py
@@ -1,6 +1,8 @@
 import requests
 
 url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'experience':2, 'test_score':9, 'interview_score':6})
+r = requests.post(url,json={'gender_Male':1,'gender_Other':0, 'age':55, 'hypertension':0, 'heart_disease':0,
+                            'avg_glucose_level':113.45, 'bmi':27.9,'smoking_status_never smoked':0,
+                            'smoking_status_smokes':0})
 
 print(r.json())
